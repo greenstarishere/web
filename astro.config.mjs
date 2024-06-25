c5169b550b68b4309159b29f3eb32d244238c081
greenstar <89572173+greenstarishere@users.noreply.github.com>
2024-06-25 13:29:34 +0700

    commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    config: {
+      applyBaseStyles: false
+    }
+  }), preact()],
+  output: 'server',
+  adapter: vercel()
+});
